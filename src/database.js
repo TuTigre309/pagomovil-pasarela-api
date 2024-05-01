40adec8b376a3847455207bc74b5e21d908b15a5
Andres De Leon <andresbarrosocl@gmail.com>
2024-05-01 12:54:12 -0400

    [FEATURE] Adherí todas las funcionalidades serverless para que funcione con lambda

--- src/database.js
+++ src/database.js
@@ -1,12 +1,28 @@
-import mongoose from "mongoose"
-import * as initialSetup from './libs/initialSetup'
+import mongoose from "mongoose";
+import * as initialSetup from './libs/initialSetup';
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
-.then(db => console.log('DB connected'))
-.then(async () => {
-    await initialSetup.createApiKey()
-})
-.catch(err => {
-    console.log(err)
-    process.exit(1)
-})
+let cachedDb = null; // Variable para almacenar la conexión de la base de datos
+
+export async function connectToDatabase() {
+  if (cachedDb) {
+    console.log('Usando conexión existente a MongoDB.');
+    return cachedDb;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    console.log('DB connected');
+    cachedDb = db; // Almacena la conexión de la base de datos para reutilizarla
+    await initialSetup.createApiKey(); // Inicializa tu configuración
+    return db;
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+export async function handler(context) {
+  context.callbackWaitsForEmptyEventLoop = false; // Evita que Lambda espere a que el event loop esté vacío
+
+  await connectToDatabase(); // Conecta a la base de datos
+}
--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
-import app from './app'
-import './database'
+import app from './app';
+import serverless from 'serverless-http'; // Importa serverless-http
+import { connectToDatabase, handler as databaseHandler } from './database'; // Importa el handler de la base de datos
 
-const port = process.env.PORT || 443;
+// Verifica si estamos corriendo en un entorno de AWS Lambda
+const isRunningOnLambda = !!process.env.LAMBDA_TASK_ROOT;
 
-app.listen(port, () => {
-    console.log(`Server listening to https://localhost:${port}/`)
-})
+// Si no estamos en Lambda, inicia el servidor como de costumbre
+if (!isRunningOnLambda) {
+  const port = process.env.PORT || 443;
+  
+  connectToDatabase().then(() => {
+    app.listen(port, () => {
+        console.log(`Server listening to https://localhost:${port}/`);
+      });
+  })
+} else {
+  // Para Lambda, utiliza el handler de la base de datos para manejar las solicitudes
+  exports.handler = async (event, context) => {
+    // Asegúrate de que la conexión a la base de datos esté lista antes de manejar la solicitud
+    await databaseHandler(event, context);
+    // Luego, pasa el control a serverless-http para manejar la solicitud de Express
+    return serverless(app)(event, context);
+  };
+}
